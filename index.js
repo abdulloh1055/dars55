f644b5aa5367de38813cef99afaa80b2093487ab
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-04-22 11:12:34 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,33 @@
+let word = prompt("hafta kunin kiriting");
+
+if(word === "monday") {
+    alert("dushamba");
+}
+
+
+if(word === "tuesday") {
+    alert("seshamba");
+}
+
+if(word === "wednesday") {
+    alert("chorshamba");
+}
+
+if(word === "throsday") {
+    alert("pashamba");
+}
+
+if(word === "friday") {
+    alert("juma");
+}
+
+if(word === "saturday") {
+    alert("shamba");
+}
+
+if(word === "sounday") {
+    alert("yakshamba");
+}
+
+
+   
